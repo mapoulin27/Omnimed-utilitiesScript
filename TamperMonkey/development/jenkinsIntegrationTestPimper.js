4528b1ee02ae994e7a7e2d030565a018b3cfddd9
Marc-Olivier Cormier <mcormier@omnimed.com>
2021-01-08 10:11:00 -0500

    Update jenkinsIntegrationTestPimper.js

--- TamperMonkey/development/jenkinsIntegrationTestPimper.js
+++ TamperMonkey/development/jenkinsIntegrationTestPimper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jenkins Integration Test List Pimper
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      3.0
 // @description  Jenkins Integration Test List Pimper
 // @author       mcormier
 // @match        https://jenkins.omnimed.com/*/job/*/job/*/*/
@@ -17,30 +17,53 @@ function getElementByXpath(path) {
           return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-
 function parseAndCleanList(tableId){
+    var listTag = [];
     var list = getElementByXpath("/html/body[@id='jenkins']/div[@id='page-body']/div[@id='main-panel']/table/tbody/tr[" + tableId + "]/td[2]/ul")
+    var nbCuke = 0
+    var nbTestIntegration = 0
     try {
         for (var i = 0; i < list.children.length; i++) {
         var currentChildNode = list.childNodes[i]
         var currentChildNodeText = currentChildNode.textContent
         if((currentChildNodeText.includes("Integration Test / healthrecord") & currentChildNodeText.includes("---")) || (currentChildNodeText.includes("Integration Test / frontend-healthrecord") & currentChildNodeText.includes("---"))) {
-            deleteCukeFromList(currentChildNode)
+            listTag[nbCuke] = getTagOfCuke(currentChildNode);
+            deleteCukeFromList(currentChildNode);
+             nbCuke++
         } else{
             buildNewElementList(currentChildNode);
             reduceSizeOfElement(currentChildNode);
+            nbTestIntegration++;
                }
         }
+        buildNewPage(list,nbCuke,nbTestIntegration,listTag);
     } catch (error) {
+
     }
+}
+function getTagOfCuke(element){
+    var tag = element.textContent
+    tag = tag.replace(element.textContent, element.textContent.match("[^\/]*?---"));
+    tag = tag.replace("---","");
+    return tag.trim();
+    }
+
+function buildNewPage(element,nbCuke,nbIT,tagList){
+    var result = tagList.reduce((r,c) => (r[c] = (r[c] || 0) + 1, r), {});
+    var tagsArray = [];
+    for (var tag in result){
+      tagsArray.push([tag + " : " + result[tag]] + "<br />")
+    }
+    var parent = element.offsetParent
+    parent.outerHTML = parent.outerHTML.replace(")", ")<br /><br /></a><b>" + nbCuke + " cukes en erreur :</b><br />- @" + tagsArray.join('- @') + "<br /><b>" + nbIT + " tests d'intégrations en erreur :</b> <br />" );
 }
 
 function deleteCukeFromList(element) {
-    element.innerHTML = '';
-    }
+     element.innerHTML = "";
+}
 
 function buildNewElementList(element) {
-    element.innerHTML = '- ' + element.innerHTML.replace(element.textContent, element.textContent.match("[^\/]*$"));
+    element.innerHTML = element.innerHTML.replace(element.textContent, element.textContent.match("[^\/]*$"));
 }
 
 function reduceSizeOfElement(element) {
@@ -48,6 +71,7 @@ function reduceSizeOfElement(element) {
 }
 
 openAllFailedTest();
+//depending on the page the list can be on various postions
 parseAndCleanList(0);
 parseAndCleanList(1);
 parseAndCleanList(2);
