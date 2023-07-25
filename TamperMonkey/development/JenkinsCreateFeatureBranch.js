37e1c77f764f61a7b777b7a4d88d3f099cccc833
Mathieu Plante Bouchard <mplante@omnimed.com>
2023-07-25 08:35:09 -0400

    Ajout JenkinsCreateFeatureBranch script
    * Ajoute un compteur sous le nom de la branche afin de savoir la longeur du nom rapidement.

---
+++ TamperMonkey/development/JenkinsCreateFeatureBranch.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Jenkins create feature branch
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @author       mplante
+// @match        https://jenkins.omnimed.com/job/CreateFeatureBranch/build?delay=0sec
+// @match        https://jenkins.omnimed.com/*/job/CreateFeatureBranch/build?delay=0sec
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=omnimed.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var currentLength = 0
+    var branchNameInput = document.getElementsByName('value')[0];
+    var div = document.createElement('div');
+    var maxLength = 39;
+
+    branchNameInput.addEventListener("input", updateLength);
+
+    div.innerHTML = "<div id='branchNameLength'></div>";
+    document.getElementsByName('parameter')[0].appendChild(div);
+
+    function updateLength(e) {
+        currentLength = e.target.value.length;
+        document.getElementById("branchNameLength").innerHTML = currentLength + "/" + maxLength;
+    };
+
+    document.getElementById("branchNameLength").innerHTML = branchNameInput.value.length + "/" + maxLength;
+
+})();
